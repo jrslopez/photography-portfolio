d4f282a3c2d9d01c8e71ea9c49069e079db04c22
James Lopez <jamesrussel.lopez@cix.csi.cuny.edu>
2024-07-20 18:51:57 -0400

    added lightbox to album images

--- src/components/Albums.js
+++ src/components/Albums.js
@@ -1,14 +1,30 @@
-import React, { Fragment } from "react"
+import React, { Fragment, useState } from "react"
 import { useParams } from "react-router-dom"
 import useFetchImages from "./useFetchImages"
 import { Gallery } from "react-grid-gallery"
+import Lightbox from "yet-another-react-lightbox"
+import "yet-another-react-lightbox/styles.css"
 
 const Album = () => {
+  const [open, setOpen] = useState(false)
+  const [currentIndex, setCurrentIndex] = useState(0)
   const images = useFetchImages()
 
   const { albumName } = useParams()
 
   const albumImages = images.filter((image) => image.albumName === albumName)
+
+  const handleClick = (index) => {
+    setOpen(true)
+    setCurrentIndex(index)
+  }
+
+  const slides = albumImages.map((image) => {
+    console.log("image url", image.url)
+    return {
+      src: image.src,
+    }
+  })
 
   return (
     <Fragment>
@@ -22,9 +38,20 @@ const Album = () => {
             rowHeight={350}
             enableImageSelection={false}
             margin={10}
+            onClick={() => setOpen(true)}
           />
         </div>
       </div>
+
+      {/* test */}
+
+      <Lightbox
+        // index={index}
+        open={open}
+        close={() => setOpen(false)}
+        slides={slides}
+        currentIndex={currentIndex}
+      />
     </Fragment>
   )
 }
