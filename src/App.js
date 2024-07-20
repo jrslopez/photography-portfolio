ceaff43207d5336061172bf913199dbd45de1be4
James Lopez <jamesrussel.lopez@cix.csi.cuny.edu>
2024-07-20 00:05:41 -0400

    Added routes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
-import Navbar from "./components/navbar"
-import Logo from "./components/logo"
-import Icons from "./components/icons"
 import "bootstrap/dist/css/bootstrap.css"
 import "./App.css"
-import Images from "./components/images"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import Navbar from "./components/navbar"
+import Home from "./components/Home"
+import About from "./components/About"
+import Contact from "./components/Contact"
 
 function App() {
   return (
-    <div>
-      <Navbar />
-      <Logo />
-      <Icons />
-      <Images />
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        <div>
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/about" element={<About />} />
+            <Route exact path="/contact" element={<Contact />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   )
 }
 
---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import Logo from "./logo"
+import Icons from "./icons"
+import Images from "./images"
+import React, { Fragment } from "react"
+
+const Home = () => {
+  return (
+    <Fragment>
+      <Logo />
+      <Icons />
+      <Images />
+    </Fragment>
+  )
+}
+
+export default Home
