8394980a7a5d50822842473791a09ff5e58c6235
James Lopez <jamesrussel.lopez@cix.csi.cuny.edu>
2024-07-20 21:21:39 -0400

    lightbox function now working

--- src/components/Albums.js
+++ src/components/Albums.js
@@ -6,21 +6,14 @@ import Lightbox from "yet-another-react-lightbox"
 import "yet-another-react-lightbox/styles.css"
 
 const Album = () => {
-  const [open, setOpen] = useState(false)
-  const [currentIndex, setCurrentIndex] = useState(0)
+  const [index, setIndex] = useState(-1)
   const images = useFetchImages()
 
   const { albumName } = useParams()
 
   const albumImages = images.filter((image) => image.albumName === albumName)
 
-  const handleClick = (index) => {
-    setOpen(true)
-    setCurrentIndex(index)
-  }
-
   const slides = albumImages.map((image) => {
-    console.log("image url", image.url)
     return {
       src: image.src,
     }
@@ -38,20 +31,19 @@ const Album = () => {
             rowHeight={350}
             enableImageSelection={false}
             margin={10}
-            onClick={() => setOpen(true)}
+            onClick={(e) => {
+              setIndex(e)
+            }}
+          />
+
+          <Lightbox
+            slides={slides}
+            open={index >= 0}
+            index={index}
+            close={() => setIndex(-1)}
           />
         </div>
       </div>
-
-      {/* test */}
-
-      <Lightbox
-        // index={index}
-        open={open}
-        close={() => setOpen(false)}
-        slides={slides}
-        currentIndex={currentIndex}
-      />
     </Fragment>
   )
 }
