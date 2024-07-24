b86d74020f6c3836fc7960b736f46c2a4451263c
James Lopez <jamesrussel.lopez@cix.csi.cuny.edu>
2024-07-24 18:42:29 -0400

    Working upload feature

--- src/components/Upload.js
+++ src/components/Upload.js
@@ -1,39 +1,60 @@
-import React, { Fragment } from "react"
+import React, { useState, Fragment } from "react"
 
 const Upload = () => {
+  const [imageURL, setImageURL] = useState("")
+  const [albumName, setAlbumName] = useState("")
+  const [date, setDate] = useState("")
+  const [front, setFront] = useState("")
+
+  const onSubmitForm = async (e) => {
+    try {
+      const body = { album_name: albumName, date, url: imageURL, front }
+      const response = await fetch("http://localhost:4000/upload", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(body),
+      })
+    } catch (error) {
+      console.error(error.message)
+    }
+  }
+
   return (
     <Fragment>
-      <h1>Upload here!</h1>
-      <div className="input-group mb-3">
-        <span className="input-group-text" id="basic-addon1">
-          User:
-        </span>
+      <h1 className="text-center mt-5">Upload Album Information</h1>
+      <form className="d-flex flex-column mt-5" onSubmit={onSubmitForm}>
         <input
           type="text"
-          className="form-control"
-          placeholder="Type username here"
-          aria-describedby="basic-addon1"
+          className="form-control mb-2"
+          placeholder="Image URL"
+          value={imageURL}
+          onChange={(e) => setImageURL(e.target.value)}
         />
-      </div>
-
-      <div className="input-group mb-3">
-        <span className="input-group-text" id="basic-addon1">
-          Pass:
-        </span>
         <input
           type="text"
-          className="form-control"
-          placeholder="Type password here"
-          aria-describedby="basic-addon1"
+          className="form-control mb-2"
+          placeholder="Album Name"
+          value={albumName}
+          onChange={(e) => setAlbumName(e.target.value)}
         />
-      </div>
-
-      <div class="input-group mb-3">
-        <input type="file" class="form-control" id="inputGroupFile02" />
-        <label class="input-group-text" for="inputGroupFile02">
-          Upload
-        </label>
-      </div>
+        <input
+          type="date"
+          className="form-control mb-2"
+          value={date}
+          onChange={(e) => setDate(e.target.value)}
+        />
+        <select
+          className="form-select"
+          aria-label="Default select example"
+          value={front}
+          onChange={(e) => setFront(e.target.value)}
+        >
+          <option value="">Put in Home Page?</option>
+          <option value="true">Yes</option>
+          <option value="false">No</option>
+        </select>
+        <button className="btn btn-success mt-2">Add</button>
+      </form>
     </Fragment>
   )
 }
