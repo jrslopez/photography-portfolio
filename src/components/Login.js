aa5cce38974ed3d64341c91341765b0bfac58292
James Lopez <jamesrussel.lopez@cix.csi.cuny.edu>
2024-08-06 23:16:59 -0400

    removed modal

--- src/components/Login.js
+++ src/components/Login.js
@@ -31,72 +31,35 @@ const Login = () => {
 
   return (
     <Fragment>
-      <button
-        type="button"
-        className="btn btn-primary"
-        data-bs-toggle="modal"
-        data-bs-target="#exampleModal"
-      >
-        Sign in here
-      </button>
-
-      <div
-        className="modal fade"
-        id="exampleModal"
-        tabIndex="-1"
-        aria-labelledby="exampleModalLabel"
-        aria-hidden="true"
-      >
-        <div className="modal-dialog">
-          <div className="modal-content">
-            <div className="modal-header">
-              <h1 className="modal-title fs-5" id="exampleModalLabel">
-                Enter Username and Password
-              </h1>
-              <button
-                type="button"
-                className="btn-close"
-                data-bs-dismiss="modal"
-                aria-label="Close"
-              ></button>
-            </div>
-            <div className="modal-body">
-              <div>
-                <form
-                  className="d-flex flex-column mt-5"
-                  onSubmit={onSubmitForm}
-                >
-                  <input
-                    type="text"
-                    className="form-control mb-2"
-                    placeholder="User Name"
-                    value={user}
-                    onChange={(e) => setUser(e.target.value)}
-                  />
-                  <input
-                    type="password"
-                    className="form-control mb-2"
-                    placeholder="Password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                  />
-                  <div className="modal-footer">
-                    <button
-                      type="button"
-                      className="btn btn-secondary"
-                      data-bs-dismiss="modal"
-                    >
-                      Close
-                    </button>
-                    <button type="submit" className="btn btn-primary">
-                      Save changes
-                    </button>
-                  </div>
-                </form>
-              </div>
-            </div>
+      <div>
+        <form className="d-flex flex-column mt-5" onSubmit={onSubmitForm}>
+          <input
+            type="text"
+            className="form-control mb-2"
+            placeholder="User Name"
+            value={user}
+            onChange={(e) => setUser(e.target.value)}
+          />
+          <input
+            type="password"
+            className="form-control mb-2"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <div className="modal-footer">
+            <button
+              type="button"
+              className="btn btn-secondary"
+              data-bs-dismiss="modal"
+            >
+              Close
+            </button>
+            <button type="submit" className="btn btn-primary">
+              Save changes
+            </button>
           </div>
-        </div>
+        </form>
       </div>
     </Fragment>
   )
